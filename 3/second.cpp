a5d772990d2b87f80ea06bb8a3f3bd0d009316eb
tonymontana <samuele.premori@gmail.com>
2024-12-03 15:12:27 +0100

    x

--- 3/second.cpp
+++ 3/second.cpp
@@ -3,7 +3,7 @@
 #include <regex>
 #include <string>
 
-std::string CleanString(std::string s) {
+std::string CleanString(std::string &s) {
   std::regex doo(R"(do\(\))");
   std::regex dont(R"(don't\(\))");
   bool clean = false;
@@ -15,32 +15,58 @@ std::string CleanString(std::string s) {
       std::string f = match.suffix();
       if (std::regex_search(f, match, doo)) {
         s = match.suffix();
+      } else {
+        clean = true;
+        clean_s.append(f);
       }
     } else {
       clean = true;
+      clean_s.append(s);
     }
   }
   return clean_s;
 }
-int main() {
-  std::ifstream myfile("test.txt");
-  std::string l;
-  int total = 0;
 
-  while (std::getline(myfile, l)) {
-    std::regex mul_pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
-    bool suffix = true;
-    std::smatch match;
-    l = CleanString(l);
-    while (suffix) {
-      if (std::regex_search(l, match, mul_pattern)) {
-        total += std::stoi(match[1]) * std::stoi(match[2]);
-        l = match.suffix();
-      } else {
-        suffix = false;
-      }
+std::string takeInput() {
+  std::ifstream file("input.txt");
+  std::istream_iterator<char> input(file);
+  std::istream_iterator<char> input_end;
+  std::string text;
+  auto inserter = std::back_inserter(text);
+  std::copy(input, input_end, inserter);
+  return text;
+}
+int main() {
+  std::string input = takeInput();
+  input = CleanString(input);
+  // std::ifstream myfile("input.txt");
+  // std::string l;
+  int total = 0;
+  std::regex mul_pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
+  bool suffix = true;
+  std::smatch match;
+  while (suffix) {
+    if (std::regex_search(input, match, mul_pattern)) {
+      total += std::stoi(match[1]) * std::stoi(match[2]);
+      input = match.suffix();
+    } else {
+      suffix = false;
     }
   }
+
+  // while (std::getline(myfile, l)) {
+  //   std::regex mul_pattern(R"(mul\((\d{1,3}),(\d{1,3})\))");
+  //   bool suffix = true;
+  //   std::smatch match;
+  //   while (suffix) {
+  //     if (std::regex_search(l, match, mul_pattern)) {
+  //       total += std::stoi(match[1]) * std::stoi(match[2]);
+  //       l = match.suffix();
+  //     } else {
+  //       suffix = false;
+  //     }
+  //   }
+  // }
   std::cout << "total: " << total << '\n';
   return 0;
 }
